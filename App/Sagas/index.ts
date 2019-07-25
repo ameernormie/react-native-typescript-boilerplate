9412dadabf849bbab59c2819d92ed2bd0d70a9f6
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-25 12:51:46 +0500

    chore: use api in startup sagas and call a dummy server

--- App/Sagas/index.ts
+++ App/Sagas/index.ts
@@ -1,6 +1,5 @@
 import { takeLatest, all } from 'redux-saga/effects';
-import API from '../Services/Api';
-import DebugConfig from '../Config/DebugConfig';
+import Api from '../Services/Api';
 
 /* ------------- Types ------------- */
 
@@ -14,13 +13,12 @@ import { startup } from './StartupSagas';
 
 // The API we use is only used from Sagas, so we create it here and pass along
 // to the sagas which need it.
-const api = API.create();
 
 /* ------------- Connect Types To Sagas ------------- */
 
 export default function* root() {
   yield all([
     // some sagas only receive an action
-    takeLatest(StartupTypes.STARTUP, startup),
+    takeLatest(StartupTypes.STARTUP, startup, Api),
   ]);
 }
