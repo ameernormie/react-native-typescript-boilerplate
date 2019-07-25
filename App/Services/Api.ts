4d70f358c72c0d413cb5a0c09fb0a9109216f0e9
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-25 12:50:54 +0500

    refactor: integrate axios config in Api.ts

--- App/Services/Api.ts
+++ App/Services/Api.ts
@@ -1,64 +1,63 @@
-// a library to wrap and simplify api calls
-import axios from 'axios';
+import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
+// TODO put this url later
+import AppConfig from '../Config/AppConfig';
+import assign from 'lodash/assign';
+import isNull from 'lodash/isNull';
+import { UserCredentials } from 'react-native-keychain';
+import AuthenticationManager from '../Lib/Keychain/AuthenticationManager';
 
-// our "constructor"
-const create = (baseURL = 'https://api.github.com/') => {
-  // ------
-  // STEP 1
-  // ------
-  //
-  // Create and configure an axios-based api object.
-  //
-  const api = axios.create({
-    // base URL is read from the "constructor"
-    baseURL,
-    // here are some default headers
-    headers: {
-      'Cache-Control': 'no-cache',
-    },
-    // 10 second timeout...
-    timeout: 10000,
-  });
+/**
+ * Assigns headers (Authorization) to axios config
+ *
+ * @method assignHeaders
+ *
+ * @param {AxiosRequestConfig} config
+ *
+ * @returns {Promise<AxiosRequestConfig>}
+ */
+export const assignHeaders = (
+  config: AxiosRequestConfig,
+): Promise<AxiosRequestConfig> => {
+  const manager = new AuthenticationManager();
 
-  // ------
-  // STEP 2
-  // ------
-  //
-  // Define some functions that call the api.  The goal is to provide
-  // a thin wrapper of the api layer providing nicer feeling functions
-  // rather than "get", "post" and friends.
-  //
-  // I generally don't like wrapping the output at this level because
-  // sometimes specific actions need to be take on `403` or `401`, etc.
-  //
-  // Since we can't hide from that, we embrace it by getting out of the
-  // way at this level.
-
-  const getRoot = () => api.get('');
-  const getRate = () => api.get('rate_limit');
-  const getUser = (username) => api.get('search/users', { q: username });
-
-  // ------
-  // STEP 3
-  // ------
-  //
-  // Return back a collection of functions that we would consider our
-  // interface.  Most of the time it'll be just the list of all the
-  // methods in step 2.
-  //
-  // Notice we're not returning back the `api` created in step 1?  That's
-  // because it is scoped privately.  This is one way to create truly
-  // private scoped goodies in JavaScript.
-  //
-  return {
-    // a list of the API functions from step 2
-    getRoot,
-    getRate,
-    getUser,
-  };
+  return manager
+    .get()
+    .then((credentials: UserCredentials | null) =>
+      // If there is no keychain entry for jwt token
+      // Then it's safe to assume that we're hitting an unprotected endpoint.
+      isNull(credentials)
+        ? config
+        : assign({}, config, {
+            headers: {
+              // credentials.password -> jwt token
+              Authorization: `JWT ${credentials.password}`,
+            },
+          }),
+    )
+    .catch(() => config);
 };
 
-// let's return back our create method as the default.
-export default {
-  create,
+/**
+ * Wraps axios (http-client)
+ *
+ * @param {AxiosRequestConfig}
+ *
+ * @returns {AxiosPromise}
+ */
+export default (config: AxiosRequestConfig): AxiosPromise => {
+  const updatedConfig = assign({}, config, {
+    /** TODO - Put api url later */
+    url: `${AppConfig.BASE_URL}${config.url}`,
+  });
+
+  if (
+    updatedConfig.url.includes('api/mobile/v1/user') ||
+    updatedConfig.url.includes('api-token-auth')
+  ) {
+    // TODO : api module should not know about any urls and this should be handled outside of this module
+    return axios(updatedConfig);
+  }
+  return assignHeaders(updatedConfig).then(
+    (configWithHeaders: AxiosRequestConfig) => axios(configWithHeaders),
+  );
 };
