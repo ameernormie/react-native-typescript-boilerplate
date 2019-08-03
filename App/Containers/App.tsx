bea2cc3aa4945ce8922a73b168c5fdca5da4c771
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-08-03 07:11:17 +0500

    chore: call startup in root container

--- App/Containers/App.tsx
+++ App/Containers/App.tsx
@@ -33,7 +33,6 @@ export const init = (store) => {
  * We separate like this to play nice with React Native's hot reloading.
  */
 const App = (props) => {
-  console.tron.log('in app ', store);
   return (
     <Provider store={store}>
       <RootContainer />
