37deb5e5dd0fc931540d9957ad5f16d329da4c61
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-08-09 00:25:59 +0500

    chore: add react-native.config.js for linking
    Previously if we had to link something customly we put it in the
    package.json and with the help of  command we linked the things
    we wanted to by running react-native link. but after the release of version 0.60
    we have to create a file at root directory named  and give
    the path of assets in there. then we run the react-native link command

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // project: {
+  //   ios: {},
+  //   android: {}, // grouped into "project"
+  // },
+  assets: ['./Assets/Fonts'], // stays the same
+  // commands: require('./path-to-commands.js'), // formerly "plugin", returns an array of commands
+};
